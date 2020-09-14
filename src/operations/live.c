cb1b9eae7389d3de0f4c4cf10f7069e4a7d9b388
eutienne <eutienne@DESKTOP-GI3JSFV.localdomain>
2020-09-14 12:58:48 +0200

    live

--- src/operations/live.c
+++ src/operations/live.c
@@ -6,7 +6,7 @@
 /*   By: eutrodri <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/30 15:53:15 by anonymous     #+#    #+#                 */
-/*   Updated: 2020/09/14 12:47:19 by eutrodri      ########   odam.nl         */
+/*   Updated: 2020/09/14 12:58:30 by eutrodri      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		live(t_cursor *c, t_game *cw)
 	c->live = cw->cycles_cnt;
 	if (c->ins->arg1 == c->reg[1] &&
 		(-(c->ins->arg1) > 0 && -(c->ins->arg1) <= cw->players))
-		cw->player_l_alive = c->ins->arg1;
+		cw->player_l_alive = -(c->ins->arg1);
 	else
 		return (0);
 	if (cw->v != NULL)
